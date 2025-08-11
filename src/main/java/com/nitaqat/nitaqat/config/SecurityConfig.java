12b8a2505a0f5e4fa97484ddcd44ab96f82c90da
AhmedAbdelalim <am909962@gami.com>
2025-08-12 00:26:33 +0300

    fix the login in dashboard

--- src/main/java/com/nitaqat/nitaqat/config/SecurityConfig.java
+++ src/main/java/com/nitaqat/nitaqat/config/SecurityConfig.java
@@ -76,5 +76,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
---
+++ src/main/java/com/nitaqat/nitaqat/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.nitaqat.nitaqat.security;
+
+import com.nitaqat.nitaqat.entity.User;
+import com.nitaqat.nitaqat.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("admin")
+                .build();
+    }
+}
